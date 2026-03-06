45585cb315b00c183867cfe5b702d0093d31ffbd
AlaAydi <contact.aydiala@gmail.com>
2026-03-06 10:04:48 +0100

    addinf and edited routes

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,35 @@ return "Hello $id";
 
 
 // addig fucntion sum two numbers
-Route::get ("/sum/{a}/{b}",  function ($a, $b){
-    return $a + $b;
+Route::get("/sum/{a}/{b}", function ($a, $b) {
+    $sum = $a + $b;
+    return "La somme de $a et $b est : $sum";
+});
+Route::get("/age/{age}", function ($age) {
+    if ($age >= 18) {
+        return "Vous êtes majeur.";
+    } else {
+        return "Vous êtes mineur.";
+    }
+});
+
+
+Route::get('/equipe/{membre?}', function ($membre = null) {
+
+    $equipe = [
+        "ali",
+        "sara",
+        "mohamed",
+        "yasmine"
+    ];
+
+    if ($membre === null) {
+        return "Toute l'équipe";
+    }
+
+    if (in_array($membre, $equipe)) {
+        return "Membre de l'équipe : " . $membre;
+    }
+
+    return "Ce membre n'existe pas";
 });
